a6730eae4dd4c8e62b35c4ef2d36df5ed5ca77a6
andrekir <andrekir@pm.me>
2023-08-01 17:56:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import android.content.IntentFilter
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    private val meshNodeChangeReceiver = MeshNodeChangeReceiver()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val filter = IntentFilter("com.geeksville.mesh.NODE_CHANGE")
+        registerReceiver(meshNodeChangeReceiver, filter)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MeshNodeChangeReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.geeksville.mesh.NodeInfo
+
+class MeshNodeChangeReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == "com.geeksville.mesh.NODE_CHANGE") {
+            // Perform actions or processing here based on the received broadcast intent
+            // For example, you can extract data from the intent using extras
+            val info: NodeInfo? = intent.getParcelableExtra("com.geeksville.mesh.NodeInfo")
+            println("received $info")
+        }
+    }
+}
