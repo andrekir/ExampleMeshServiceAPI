9bb0b9fccdf0c8b5eb4948e19d2e636834a8a803
andrekir <andrekir@pm.me>
2023-08-05 07:56:37 -0300

    add `bindService` basics

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,15 +14,48 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.content.IntentFilter
+import android.content.ServiceConnection
+import android.os.IBinder
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.geeksville.mesh.IMeshService
 
 class MainActivity : ComponentActivity() {
     private val meshNodeChangeReceiver = MeshNodeChangeReceiver()
+
+    // Service connection to handle the communication with the service
+    private var meshService: IMeshService? = null
+    private var isServiceBound = false
+
+    // Define the service connection
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            // Service is connected. Get the binder and cast it to our service
+            meshService = IMeshService.Stub.asInterface(service)
+            isServiceBound = true
+            // You can now call methods on the MeshService instance if needed
+            // ie. meshService.send(datapacket)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            // Service disconnected, clean up any references
+            isServiceBound = false
+            meshService = null
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val filter = IntentFilter("com.geeksville.mesh.NODE_CHANGE")
         registerReceiver(meshNodeChangeReceiver, filter)
+
+        val intent = Intent().apply {
+            setClassName(
+                "com.geeksville.mesh",
+                "com.geeksville.mesh.service.MeshService"
+            )
+        }
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
