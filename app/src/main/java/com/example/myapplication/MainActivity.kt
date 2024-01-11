4db1e9a3b21ad2410ff4c1144b9a4cc2d538e199
andrekir <andrekir@pm.me>
2024-01-11 12:21:30 -0300

    feat: receive `TEXT_MESSAGE_APP` DataPackets

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -45,7 +45,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val filter = IntentFilter("com.geeksville.mesh.NODE_CHANGE")
+        val filter = IntentFilter().apply {
+            addAction("com.geeksville.mesh.NODE_CHANGE")
+            addAction("com.geeksville.mesh.RECEIVED.TEXT_MESSAGE_APP")
+        }
         registerReceiver(meshNodeChangeReceiver, filter)
 
         val intent = Intent().apply {
